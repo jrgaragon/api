848dd0dde44b6305d359b480a70ed32a23ec96ec
Jorge Aragon <jrg.aragon@gmail.com>
2019-08-13 20:38:42 -0500

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+server.post('/LiteResponseV2', (request, response) => {
+    console.log(request.body);
+    response.json({
+        result: {
+            sourceEntityId: "" + request.body.result.sourceEntityId,
+            destinationEntityId: "" + request.body.result.destinationEntityId,
+            message: "" + new Date(),
+            status: "success"
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log('listening at port', port)
+});
